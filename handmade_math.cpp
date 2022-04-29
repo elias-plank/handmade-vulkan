68c2f23b458170e6575e896ddc7308e369807cff
PlanEl180223 <40146720180223@litec.ac.at>
2022-04-29 20:42:25 +0200

    add some math

--- handmade_math.cpp
+++ handmade_math.cpp
@@ -1,19 +1,81 @@
 #include "handmade_math.h"
 
 namespace handmade {
-
+	
 	f64 Min(f64 a, f64 b) {
-
+		
 		return a < b ? a : b;
 	}
-
+	
 	f64 Max(f64 a, f64 b) {
-
+		
 		return a > b ? a : b;
 	}
-
+	
 	f64 Clamp(f64 x, f64 upper, f64 lower) {
-
+		
 		return Min(upper, Max(x, lower));
+	}
+	
+	Vector3 Add(Vector3* left, Vector3* right) {
+		
+		Vector3 result{};
+		result.X = left->X + right->X;
+		result.Y = left->Y + right->Y;
+		result.Z = left->Z + right->Z;
+		
+		return result;
+	}
+	
+	Vector3 Sub(Vector3* left, Vector3* right) {
+		
+		Vector3 result{};
+		result.X = left->X - right->X;
+		result.Y = left->Y - right->Y;
+		result.Z = left->Z - right->Z;
+		
+		return result;
+	}
+	
+	Vector3 Mult(Vector3* left, Vector3* right) {
+		
+		Vector3 result{};
+		result.X = left->X * right->X;
+		result.Y = left->Y * right->Y;
+		result.Z = left->Z * right->Z;
+		
+		return result;
+	}
+	
+	Vector3 Div(Vector3* left, Vector3* right) {
+		
+		Vector3 result{};
+		result.X = left->X / right->X;
+		result.Y = left->Y / right->Y;
+		result.Z = left->Z / right->Z;
+		
+		return result;
+	}
+	
+	f32 Dot(Vector3* left, Vector3* right) {
+		
+		return left->X * right->X + left->Y * right->Y + left->Z * right->Z;
+	}
+	
+	f32 Length(Vector3* vector) {
+		
+		return sqrt(vector->X * vector->X + vector->Y * vector->Y + vector->Z * vector->Z);
+	}
+	
+	Vector3 Unit(Vector3* vector) {
+		
+		f32 length = Length(vector);
+		
+		Vector3 result{};
+		result.X = vector->X / length;
+		result.Y = vector->Y / length;
+		result.Z = vector->Z / length;
+		
+		return result;
 	}
 }
--- handmade_math.h
+++ handmade_math.h
@@ -3,11 +3,23 @@
 
 #include "handmade_types.h"
 
-namespace handmade {
+#include <cmath>
 
+namespace handmade {
+	
 	f64 Min(f64 a, f64 b);
 	f64 Max(f64 a, f64 b);
 	f64 Clamp(f64 x, f64 upper, f64 lower);
+	
+	Vector3 Add(Vector3* left, Vector3* right);
+	Vector3 Sub(Vector3* left, Vector3* right);
+	Vector3 Mult(Vector3* left, Vector3* right);
+	Vector3 Div(Vector3* left, Vector3* right);
+	
+	f32 Dot(Vector3* left, Vector3* right);
+	f32 Length(Vector3* vector);
+	
+	Vector3 Unit(Vector3* vector);
 }
-
+// NOTE(Elias): 
 #endif // HANDMADE_MATH_H
