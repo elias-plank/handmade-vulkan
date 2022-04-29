3a66019d04e6d213fd95bde525ece47886f35da8
PlanEl180223 <40146720180223@litec.ac.at>
2022-04-29 11:49:44 +0200

    can update buffers now

--- handmade_main.cpp
+++ handmade_main.cpp
@@ -2,55 +2,82 @@
 #include "handmade_window.h"
 
 namespace handmade {
-
+    
 	int Main(int argc, char** argv) {
-
+        
 		Window window{};
 		if (WindowCreate(&window, "Handmade Vulkan", 800, 600)) {
-
+            
 			// Vulkan
 			VulkanState vulkanState{};
 			if (VulkanStateInit(&vulkanState, &window)) {
-
+                
 				Vertex vertices[4] = {
-
+                    
 					{{-1.0f, -1.0f}, {1.0f, 0.0f, 0.0f}},
 					{{ 1.0f, -1.0f}, {0.0f, 1.0f, 0.0f}},
 					{{ 1.0f,  1.0f}, {0.0f, 0.0f, 1.0f}},
 					{{-1.0f,  1.0f}, {1.0f, 1.0f, 1.0f}}
 				};
-
+                
 				u32 indices[6] = {
-
+                    
 					0, 1, 2, 2, 3, 0
 				};
-
+                
 				VulkanBuffer vertexBuffer{};
 				VulkanCreateVertexBuffer(&vulkanState, &vertexBuffer, vertices, ARRAY_SIZE(vertices));
-
+                
 				VulkanBuffer indexBuffer{};
 				VulkanCreateIndexBuffer(&vulkanState, &indexBuffer, indices, ARRAY_SIZE(indices));
-
+                
+                f64 frameTime = glfwGetTime();
+                u64 inc = 0;
+                
 				while (WindowIsRunning(&window)) {
-
+                    
+                    f64 time = glfwGetTime();
+                    f64 dt = time - frameTime;
+                    frameTime = time;
+                    
+                    if (inc % 1000 == 0) {
+                        
+                        printf("fps: %lf\r", 1.0 / dt);
+                    }
+                    
+                    inc++;
+                    
+                    if (GetAsyncKeyState(VK_F4)) {
+                        
+                        Vertex adjustedVertices[4] = {
+                            
+                            {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+                            {{ 0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+                            {{ 0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}},
+                            {{-0.5f,  0.5f}, {1.0f, 1.0f, 1.0f}}
+                        };
+                        
+                        VulkanVertexBufferSetData(&vulkanState, &vertexBuffer, adjustedVertices, 4);
+                    }
+                    
 					VulkanDrawIndexed(&vulkanState, &window, &vertexBuffer, &indexBuffer, ARRAY_SIZE(indices));
 					WindowUpdate(&window);
 				}
-
+                
 				VulkanDestroyVertexBuffer(&vulkanState, &vertexBuffer);
 				VulkanDestroyIndexBuffer(&vulkanState, &indexBuffer);
 			}
-
+            
 			VulkanStateDestroy(&vulkanState);
 		}
-
+        
 		WindowDestroy(&window);
-
+        
 		return 0;
 	}
 }
 
 int main(int argc, char** argv) {
-
+    
 	return handmade::Main(argc, argv);
 }
--- handmade_vulkan.h
+++ handmade_vulkan.h
@@ -12,15 +12,15 @@
 #include <cstdlib>
 
 namespace handmade {
-
+    
 	struct VulkanShaderCode {
-
+        
 		u8* Data;
 		u32 Size;
 	};
-
+    
 	struct VulkanSwapChain {
-
+        
 		VkSwapchainKHR SwapChain;
 		VkImage* Images;
 		u32 ImageCount;
@@ -28,27 +28,27 @@ namespace handmade {
 		VkExtent2D Extent;
 		VkImageView* ImageViews;
 		u32 ImageViewCount;
-
+        
 		VkFramebuffer* Framebuffers;
 		u32 FramebufferCount;
 		bool FramebufferResized;
 	};
-
+    
 	struct VulkanPipeline {
-
+        
 		VkRenderPass RenderPass;
 		VkPipelineLayout PipeLineLayout;
 		VkPipeline GraphicsPipeline;
 	};
-
+    
 	struct VulkanBuffer {
-
+        
 		VkBuffer Buffer;
 		VkDeviceMemory BufferMemory;
 	};
-
+    
 	struct VulkanState {
-
+        
 		VkInstance Instance;
 		VkDebugUtilsMessengerEXT DebugMessenger;
 		
@@ -58,54 +58,56 @@ namespace handmade {
 		
 		VkQueue GraphicsQueue;
 		VkQueue PresentQueue;
-
+        
 		VulkanSwapChain SwapChain;
 		VulkanPipeline Pipeline;
-
+        
 		VkCommandPool CommandPool;
 		VkCommandBuffer* CommandBuffers;
 		u32 CommandBufferCount;
-
+        
 		VkSemaphore* ImageAvailableSemaphores;
 		u32 ImageAvailableSemaphoreCount;
 		VkSemaphore* RenderFinishedSemaphores;
 		u32 RenderFinishedSemaphoreCount;
 		VkFence* InFlightFences;
 		u32 InFlightFenceCount;
-
+        
 		u32 CurrentFrame;
 	};
-
+    
 	struct VulkanQueueFamilyIndices {
-
+        
 		u32 GraphicsFamily;
 		u32 PresentFamily;
 		bool GraphicsComplete;
 		bool PresentComplete;
 	};
-
+    
 	struct VulkanSwapChainSupportDetails {
-
+        
 		VkSurfaceCapabilitiesKHR Capabilities;
 		VkSurfaceFormatKHR* Formats;
 		VkPresentModeKHR* PresentModes;
 		u32 FormatCount;
 		u32 PresentModeCount;
 	};
-
+    
 	// State Functions
 	bool VulkanStateInit(VulkanState* state, Window* window);
 	bool VulkanStateDestroy(VulkanState* state);
-
+    
 	// Drawing
 	bool VulkanDrawIndexed(VulkanState* state, Window* window, VulkanBuffer* vertexBuffer, VulkanBuffer* indexBuffer, u32 indexCount);
-
+    
 	bool VulkanCreateVertexBuffer(VulkanState* state, VulkanBuffer* vertexBuffer, Vertex* vertices, u32 count);
 	void VulkanDestroyVertexBuffer(VulkanState* state, VulkanBuffer* vertexBuffer);
-
+    bool VulkanVertexBufferSetData(VulkanState* state, VulkanBuffer* vertexBuffer, Vertex* vertices, u32 count);
+    
 	bool VulkanCreateIndexBuffer(VulkanState* state, VulkanBuffer* indexBuffer, u32* indices, u32 count);
 	void VulkanDestroyIndexBuffer(VulkanState* state, VulkanBuffer* indexBuffer);
-
+    bool VulkanIndexBufferSetData(VulkanState* state, VulkanBuffer* indexBuffer, u32* indices, u32 count);
+    
 	VulkanShaderCode VulkanLoadShaderCode(const char* path);
 }
 
